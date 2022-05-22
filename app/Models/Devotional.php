e3d4f228d4969b07c670e71fc133decb37989bd7
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-05-22 14:09:50 +0100

    Devotional Model and Migration worked on

--- app/Models/Devotional.php
+++ app/Models/Devotional.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Devotional extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    protected $fillable = ['devotional_date', 'topic', 'bible_text', 'memory_verse_text', 'memory_verse', 'devotional', 'further_reading', 'prayers'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom(['devotional_date', 'topic'])
+            ->saveSlugsTo('slug');
+    } 
 }
--- database/migrations/2022_05_22_110531_create_devotionals_table.php
+++ database/migrations/2022_05_22_110531_create_devotionals_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('devotionals', function (Blueprint $table) {
             $table->id();
+            $table->date('devotional_date');
+            $table->string('topic', 1000);
+            $table->string('bible_text', 255);
+            $table->string('memory_verse_text', 255);
+            $table->text('memory_verse');
+            $table->longText('devotional');
+            $table->longText('further_reading');
+            $table->text('prayers');
             $table->timestamps();
         });
     }
