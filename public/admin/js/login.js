2b6ac75eb780721ce3dca9264cefa3cf6b3b4636
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-01-15 12:17:49 +0100

    JS modified

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,4 +1,4 @@
-var BASE_URL = "https://cfcing.org/";
+var BASE_URL = "https://www.cfcing.org/";
 // var BASE_URL = "http://127.0.0.1:8000/";
 var ADMIN_URL = BASE_URL + "dashboard/";
 if (sessionStorage.getItem('token') == null || sessionStorage.getItem('token') == '') {
