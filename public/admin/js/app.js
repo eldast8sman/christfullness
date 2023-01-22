e064b1289acde2a8ff7a5cd8651deb1258180e3c
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-01-22 10:14:42 +0100

    WYSIWYG removed

--- public/admin/js/app.js
+++ public/admin/js/app.js
@@ -1609,15 +1609,15 @@ if($("table#series_table")){
 //     CKEDITOR.replace('textarea#welcome_message');
 // }
 
-$('textarea#welcome_message').tinymce({
-    height: 500,
-    menubar: false,
-    plugins: [
-      'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-      'anchor', 'searchreplace', 'visualblocks', 'fullscreen',
-      'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-    ],
-    toolbar: 'undo redo | blocks | bold italic backcolor | ' +
-      'alignleft aligncenter alignright alignjustify | ' +
-      'bullist numlist outdent indent | removeformat | help'
-  });
+// $('textarea#welcome_message').tinymce({
+//     height: 500,
+//     menubar: false,
+//     plugins: [
+//       'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+//       'anchor', 'searchreplace', 'visualblocks', 'fullscreen',
+//       'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+//     ],
+//     toolbar: 'undo redo | blocks | bold italic backcolor | ' +
+//       'alignleft aligncenter alignright alignjustify | ' +
+//       'bullist numlist outdent indent | removeformat | help'
+//   });
