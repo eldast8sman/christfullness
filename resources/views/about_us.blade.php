cc194f44072af406d6008825a729bb11b50c6b59
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-01-25 19:41:12 +0100

    About Us Done

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -17,5 +17,30 @@
             <li class="breadcrumb-item text-dark active" aria-current="page">About Us</li>
         @endslot
     @endcomponent
+
+    @foreach ($abouts as $about)
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-5 align-items-center">
+                    @if (!empty($about->filename))
+                        <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                            <div class="about-img position-relative overflow-hidden p-5 pe-0">
+                                <img class="img-fluid w-100" src="{{ $about->filename }}" alt="{{ $about->heading }}">
+                            </div>
+                        </div>   
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                            <h1 class="display-5 mb-4">{{ $about->heading }}</h1>
+                            <p class="mb-4">{!! $about->content !!}</p>
+                        </div>   
+                    @else
+                       <div class="col-12 wow fadeIn" data-wow-delay="0.5s">
+                            <h1 class="display-5 mb-4">{{ $about->heading }}</h1>
+                            <p class="mb-4">{!! $about->content !!}</p>
+                        </div> 
+                    @endif
+                </div>
+            </div>
+        </div>
+    @endforeach
     
 @endsection
