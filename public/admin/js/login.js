4a93bc521714a7318d41fd7f9bfe274cc39b8bc7
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-02-19 12:58:04 +0100

    Local Soteage Test

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,6 +1,8 @@
 var BASE_URL = "https://www.cfcing.org/";
 // var BASE_URL = "http://127.0.0.1:8000/";
 var ADMIN_URL = BASE_URL + "dashboard/";
-if (localStorage.getItem('token') == null || localStorage.getItem('token') == '') {
-    window.location = ADMIN_URL + 'login'
-}
+// if (localStorage.getItem('token') == null || localStorage.getItem('token') == '') {
+//     window.location = ADMIN_URL + 'login'
+// }
+
+console.log(localStorage);
