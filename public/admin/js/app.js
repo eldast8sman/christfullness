0bb224d3c242832379c51dc9b8a1c797e2e82565
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-02-26 05:43:50 +0100

    Local Storage

--- public/admin/js/app.js
+++ public/admin/js/app.js
@@ -38,7 +38,7 @@ $(".loginForm").submit(function(e) {
 
                     console.log(localStorage);
 
-                    // window.location = ADMIN_URL
+                    window.location = ADMIN_URL
                 } else {
                     toaster_error(response.message);
                 }
